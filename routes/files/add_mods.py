cdd342ceb5dba8324f9b7629f74b18b4747dbfdd
salmen1234 <salmendjeghem@gmail.com>
2025-03-05 23:50:27 +0100

    Added remove_mod route

---
+++ routes/files/add_mods.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import request
+from server import Server
+
+from utils.database import Database
+from utils.server_methods import send_response
+
+def route():
+    db = Database()
+    
+    server_id = request.args.get("id")
+    mods_ids = request.args.get("mods_ids").split(",")
+    
+    server = Server(server_db=db.get_server(server_id), server_id=server_id)
+    
+    if not os.path.exists(server.mods_path):
+        return send_response(content="Mods path doesn't exist, please start server first.", error="File Error", success=False, code=404)
+    
+    return server.forge_app.download_mods(ids=mods_ids)
